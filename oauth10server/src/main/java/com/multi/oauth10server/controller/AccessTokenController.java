a1b5853b12faa4ba6fc89afc402b540a38a583f1
joYoungBin <student@M40320>
2022-06-23 15:32:08 +0900

    #Day4 fixed AccesTokenController and Interceptor

--- oauth10server/src/main/java/com/multi/oauth10server/controller/AccessTokenController.java
+++ oauth10server/src/main/java/com/multi/oauth10server/controller/AccessTokenController.java
@@ -1,7 +1,9 @@
 package com.multi.oauth10server.controller;
 
+import com.multi.oauth10server.model.AccessTokenVO;
 import com.multi.oauth10server.model.ConsumerVO;
 import com.multi.oauth10server.model.RequestTokenVO;
+import com.multi.oauth10server.model.UsersVO;
 import com.multi.oauth10server.service.ConsumerService;
 import com.multi.oauth10server.service.RequestTokenService;
 import com.multi.oauth10server.service.UsersService;
@@ -11,7 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.thinker.oauth.generator.TokenGenerator;
-import org.thinker.oauth.parameter.RequestTokenParam;
+import org.thinker.oauth.parameter.AccessTokenParam;
+import org.thinker.oauth.util.OAuthMsgConstants;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -30,36 +33,28 @@ public class AccessTokenController {
 
     @GetMapping
     public ModelAndView getAccessToken(HttpServletRequest request) throws Exception {
-
-        // 1. QueryString 또는 Post 파라미터 파싱
-        RequestTokenParam param = new RequestTokenParam(request);
-
-		// 2. tbl_oauth_key 테이블에서 ConsumerSecret 정보 읽어옴.
+        // 1. 파라미터 파싱
+        AccessTokenParam param = new AccessTokenParam(request);
+        // 1.1 DB테이블에서 consumer, requestToken, User 정보 읽음(ConsumerSecret, Password,
+        // Verifier 정보 획득)
         ConsumerVO consumerVO = consumerService.selectByConsumerKey(param.getConsumerKey());
-        String consumerSecret = consumerVO.getConsumerSecret();
-
-		// 3. signature validation!! 유효하지 않으면 예외 발생!
-        param.validateRequestToken(consumerSecret);
-
-		// 4. 유효하다면 RequestToken 생성하여 tbl_request_token 테이블에 저장!!
-        RequestTokenVO tokenVO = new RequestTokenVO();
-        tokenVO.setConsumerKey(consumerVO.getConsumerKey());
-        tokenVO.setCallback(param.getCallback());
-        TokenGenerator.generateRequestToken(tokenVO);
-
-		System.out.println("## RTO : " + tokenVO);
-        requestTokenService.createRequestToken(tokenVO);
-
-		String oauth_callback_confirmed = "true";
+        RequestTokenVO requestTokenVO = requestTokenService.getRequestToken(param.getRequestToken());
+        UsersVO usersVO = usersService.selectUserByUserNo(requestTokenVO.getUserNo());
+        // 2. signature validation!! 유효하지 않으면 예외 발생!
+        param.validateRequestToken(requestTokenVO.getRequestTokenSecret(), requestTokenVO.getVerifier(),
+                consumerVO.getConsumerSecret());
+        // 2.1 유효하다면 RequestToken 테이블의 레코드 삭제 : 임시 TOken이므로..
+        requestTokenService.deleteRequestToken(requestTokenVO.getRequestToken());
+        // 3. AccessToken 생성
+        AccessTokenVO accessTokenVO = TokenGenerator.generateAccessToken(usersVO, consumerVO);
         StringBuilder builder = new StringBuilder();
-        builder.append("oauth_token=" + tokenVO.getRequestToken() + "&");
-        builder.append("oauth_token_secret=" + tokenVO.getRequestTokenSecret() + "&");
-        builder.append("oauth_callback_confirmed=" + oauth_callback_confirmed);
-
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("message", builder.toString());
-        mav.setViewName("request_token");
-
-		return mav;
+        builder.append(OAuthMsgConstants.OAUTH_TOKEN + "=" + accessTokenVO.getAccessToken() + "&");
+        builder.append(OAuthMsgConstants.OAUTH_TOKEN_SECRET + "=" + accessTokenVO.getAccessTokenSecret() + "&");
+        builder.append("userno=" + accessTokenVO.getUserNo() + "&");
+        builder.append("userid=" + accessTokenVO.getUserID());
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("access_token");
+        mav.addObject("message", builder.toString());
+        return mav;
     }
 }
--- oauth10server/src/main/java/com/multi/oauth10server/util/OAuthInterceptor.java
+++ oauth10server/src/main/java/com/multi/oauth10server/util/OAuthInterceptor.java
@@ -1,31 +1,37 @@
 package com.multi.oauth10server.util;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.multi.oauth10server.model.ConsumerVO;
+import com.multi.oauth10server.model.UsersVO;
+import com.multi.oauth10server.service.ConsumerService;
+import com.multi.oauth10server.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import org.thinker.oauth.parameter.OAuthTokenParam;
 
-import com.multi.oauth10server.model.ConsumerVO;
-import com.multi.oauth10server.model.UsersVO;
-import com.multi.oauth10server.service.ConsumerService;
-import com.multi.oauth10server.service.UsersService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Component
-public class OAuthInterceptor  extends HandlerInterceptorAdapter  {
-	@Autowired
-	UsersService usersService;
-	@Autowired
-	ConsumerService consumerService;
-	
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
-		
-		//이곳에 코드를 작성합니다.
-        
+public class OAuthInterceptor extends HandlerInterceptorAdapter {
+    @Autowired
+    UsersService usersService;
+    @Autowired
+    ConsumerService consumerService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+
+        OAuthTokenParam param = new OAuthTokenParam(request);
+        long userNo = param.getUserNo();
+        String consumerKey = param.getConsumerKey();
+        UsersVO usersVO = usersService.selectUserByUserNo(userNo);
+        ConsumerVO consumerVO = consumerService.selectByConsumerKey(consumerKey);
+        param.validateRequestToken(consumerVO, usersVO);
+        request.setAttribute("usersVO", usersVO);
+        request.setAttribute("consumerVO", consumerVO);
+
         return true;
-	}
+    }
 }
